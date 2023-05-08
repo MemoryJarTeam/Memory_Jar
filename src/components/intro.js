b0117c3728d9ad8c3c5d9fbd5486506871df71e8
Jacques Ehrardt <ehrardt38@gmail.com>
2023-05-08 16:38:37 -0700

    Intro: add html render

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import jarCharacter2 from '../image/jarCharacter-2.png'
+
 class IntroClass extends React.Component{
-   
+   render(){
+      return(
+         <section className="intro">
+            <figure>
+                <img
+                    src={jarCharacter2}
+                    alt="jarCharacter"
+                />
+                <figcaption>
+                    <h1>Memory Jar</h1>
+                </figcaption>
+            </figure>
+            <article>
+                <button className="logInBtn" onClick={this.handleLogIn} >Log In with Email</button>
+                <button className="signUpBtn" onClick={this.handleSignUp} >Sign Up Our Jar!</button>
+            </article>
+        </section>
+      )
+   }
 }
 
 export function Intro(props){
