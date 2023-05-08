08492dae41b5407ab8450861c4462289ee183b0f
Jacques Ehrardt <ehrardt38@gmail.com>
2023-05-08 16:45:54 -0700

    Add login and signUp components

---
+++ src/components/logIn.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import jarCharacter2 from '../image/jarCharacter-2.png';
+
+class LogInClass extends React.Component{
+   constructor(props){
+      super(props);
+      this.state = {
+         email: '', 
+         password: '', 
+         userList: []
+      };
+   };
+
+   componentDidMount(){
+      fetch("http://localhost:5000/user-api")
+      .then((response)=>response.json())
+      .then((userData)=>{
+         this.setState({userList: userData});
+      });
+   };
+
+   handleChange = (event) =>{
+      this.setState({[event.target.name]: event.target.value});
+   };
+
+   handleSubmit = (event) =>{
+      event.preventDefault();
+      if(this.state.email === "" || this.state.password === ""){
+         alert("Please fill in Email and Password");
+      }
+      else{
+         this.state.userList.forEach((user)=>{
+            if(user.email === this.state.email && user.password === this.state.password){
+               fetch("http://localhost:5000/user-login", {
+                  method: 'POST',
+                  headers: {
+                     'Content-type': 'application/json'
+                  },
+                  body: JSON.stringify(user)
+               }).then(function(response){
+                  return response.json();
+               });
+               this.props.navigate('/main-page');
+            }
+         });
+      }
+   };
+   
+   render(){
+      return(
+         <section className="logIn">
+            <figure>
+               <img src={jarCharacter2} alt="Jar Logo"/>
+            </figure>
+            <form onSubmit={this.handleSubmit}>
+               <aside>
+                  <input type="email" name="email" id="email" placeholder="Email" value={this.state.value} onChange={this.handleChange}/>
+                  <input type="password" name="password" id="password" placeholder="Password" value={this.state.value} onChange={this.handleChange}/>
+               </aside>
+               <input type="submit" value="Log In" />
+            </form>
+         </section>
+      )
+   }
+};
+
+export function LogIn(props){
+   const navigate = useNavigate();
+   return(<LogInClass navigate={navigate}/>)
+}
---
+++ src/components/signUp.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+import userPic from '../image/user.png'
+import imgFile from '../image/image.png'
+
+class SignUpClass extends React.Component{
+   constructor(props){
+      super(props);
+      this.state = {name: '', email: '', password: '', confirmPass: '', userList: []};
+   };
+
+   componentDidMount(){
+      fetch("http://localhost:5000/user-api")
+      .then((response)=>response.json())
+      .then((userData)=>{
+         this.setState({userList: userData});
+      });
+   };
+
+   handleChange = (event) =>{
+      this.setState({[event.target.name]: event.target.value});
+   }
+
+   handleSubmit = (event) =>{
+      let counter = 0;
+      let lastUser = this.state.userList.length-1;
+      this.state.userList.forEach((user)=>{
+         if(user.email === this.state.email){
+            alert("This user exist");
+            return false;
+         }
+         else if(counter === lastUser){
+            let newUser = {
+               "id": this.state.userList[lastUser].id+1,
+               "name": this.state.name,
+               "email": this.state.email,
+               "password": this.state.password
+            };
+            fetch("http://localhost:5000/new-user", {
+               method: 'POST',
+               headers: {
+                  'Content-type': 'application/json'
+               },
+               body: JSON.stringify(newUser)
+            }).then(function(response){
+               return response.json();
+            });
+            this.props.navigate('/sign-up');
+         }
+         counter++;
+      });
+      event.preventDefault();
+   }
+
+   render(){
+      return(
+         <section className="logIn">
+            <form onSubmit={this.handleSubmit}>
+               <figure className="userPic">
+                  <img src={userPic} alt="User Pic"/>
+                  <label htmlFor="userPic">
+                     <img src={imgFile} alt="Icon"/>
+                  </label>
+                  <input type="file" accept="image/*" name="userPic" id="userPic"/>
+               </figure>
+               <aside>
+                  <input type="text" name="name" id="name" placeholder="User Name" value={this.state.value} onChange={this.handleChange}/>
+                  <input type="email" name="email" id="email" placeholder="User Email" value={this.state.value} onChange={this.handleChange}/>
+                  <input type="password" name="password" id="password" placeholder="Password" value={this.state.value} onChange={this.handleChange}/>
+                  <input type="password" name="confirmPass" id="confirmPass"  placeholder="Confirm Password" value={this.state.value} onChange={this.handleChange}/>
+               </aside>
+               <input type="submit" value="Sign Up"/>
+            </form>
+         </section>
+      )
+   }
+}
+
+export function SignUp(props){
+   const navigate = useNavigate();
+   return(<SignUpClass navigate={navigate} />)
+}
