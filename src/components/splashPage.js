fc85ac1995685a2820c764cc38ddff17d1164bec
Dayhun <lanna05@naver.com>
2023-05-08 20:19:00 -0700

    SplashPage added

--- src/components/splashPage.js
+++ src/components/splashPage.js
@@ -1,30 +1,19 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from 'react';
-import jarCharacter2 from '../image/jarCharacter-2.png'
+import jarCharacter2 from "../image/jarCharacter-2.png";
 
 class SplashPageClass extends React.Component {
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //             this.props.navigate("/intro");
-    //     }, 2000);
-    // }, []);
-
-    componentDidMount(){
+    componentDidMount() {
         setTimeout(() => {
             this.props.navigate("/intro");
         }, 2000);
     }
-    // setTimeout(() => {
-    // }, 2000);
-
     render() {
         return (
             <section>
                 <figure class="splash">
-                    <img src={jarCharacter2} alt="Jar"/>
+                    <img src={jarCharacter2} alt="Jar" />
                 </figure>
-                {/* <script src="./script/index.js" defer></script> */}
             </section>
         );
     }
