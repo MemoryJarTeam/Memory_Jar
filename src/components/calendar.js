7f9159cfde9885483bccb9ccebd1d1af956dc565
Dayhun <lanna05@naver.com>
2023-05-11 02:51:25 -0700

    calendar page setting

---
+++ src/components/calendar.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import Calendar from "react-calendar";
+import iconArrowLeft from "../image/icon/icon-arrow-left.svg";
+
+class CalendarClass extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataIsReturned: false,
+            userInfo: [],
+            currentJarInfo: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:5000/user-loged")
+            .then((response) => response.json())
+            .then((userData) => {
+                this.setState({ userInfo: userData[0] });
+                if (userData[0].jar.length !== 0) {
+                    fetch("http://localhost:5000/current-jar")
+                        .then((response) => response.json())
+                        .then((currentJar) => {
+                            this.setState({ currentJarInfo: currentJar[0] });
+                            this.setState({ dataIsReturned: true });
+                        });
+                }
+            });
+    }
+
+    handleMainPage = () => {
+        this.props.navigate("/main-page");
+    };
+
+    handleUser = () => {
+        this.props.navigate("/user-setting");
+    };
+
+    handleKeyword = () => {
+        this.props.navigate("/keyword");
+    };
+
+    handleSelectMemory = (props) => {
+        let newCurrentMemoryId = {
+            newCurrentMemoryId: parseInt(props.target.id),
+        };
+
+        fetch("http://localhost:5000/change-current-memory", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(newCurrentMemoryId),
+        }).then(function (response) {
+            return response.json();
+        });
+        this.props.navigate("/memory");
+    };
+
+    render() {
+        if (this.state.dataIsReturned) {
+            return (
+                <section className="calendar">
+                    <nav className="backBtn">
+                        <img
+                            src={iconArrowLeft}
+                            alt="arrow-left-icon"
+                            onClick={this.handleMainPage}
+                        />
+                    </nav>
+                    <main className="calendar-main">
+                        <h3>{this.state.currentJarInfo.name}</h3>
+                        <section>
+                            <button
+                                className="calendarBtn"
+                                
+                            >
+                                Calendar
+                            </button>
+                            <button className="keywordBtn" onClick={this.handleKeyword} >Key word</button>
+                        </section>
+                        {/* <this.CalendarArticle /> */}
+                    </main>
+                    <footer>
+                        <section>
+                            <button>
+                                <i
+                                    className="fa-solid fa-house-chimney"
+                                    onClick={this.handleMainPage}
+                                ></i>
+                            </button>
+                            <button>
+                                <i
+                                    className="fa-solid fa-user"
+                                    onClick={this.handleUser}
+                                ></i>
+                            </button>
+                        </section>
+                    </footer>
+                </section>
+            );
+        } else {
+            return <h1>Loading...</h1>;
+        }
+    }
+}
+
+export function CalendarPage(props) {
+    const navigate = useNavigate();
+    return <CalendarClass navigate={navigate} />;
+}
--- src/index.js
+++ src/index.js
@@ -1,67 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { LogIn } from './components/logIn';
-import { SignUp } from './components/signUp';
-import { UserSettings } from './components/userSetting';
-import { MainPage } from './components/mainPage';
-import { AddJar } from './components/addJar';
-import { MemoryWriting } from './components/memoryWriting';
-import { Intro } from './components/intro';
-import { KeyWord } from './components/keyWord';
-import { Memory } from './components/memory';
+import { LogIn } from "./components/logIn";
+import { SignUp } from "./components/signUp";
+import { UserSettings } from "./components/userSetting";
+import { MainPage } from "./components/mainPage";
+import { AddJar } from "./components/addJar";
+import { MemoryWriting } from "./components/memoryWriting";
+import { Intro } from "./components/intro";
+import { KeyWord } from "./components/keyWord";
+import { Memory } from "./components/memory";
+import { CalendarPage } from "./components/calendar";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route
-        path="/"
-        element={<App/>}
-      />
-      <Route
-        path="/intro"
-        element={<Intro/>}
-      />
-      <Route
-        path="/login"
-        element={<LogIn/>}
-      />
-      <Route
-        path="/sign-up"
-        element={<SignUp/>}
-      />
-      <Route
-        path="/user-setting"
-        element={<UserSettings/>}
-      />
-      <Route
-        path="/main-page"
-        element={<MainPage/>}
-      />
-      <Route
-        path="/new-jar"
-        element={<AddJar/>}
-      />
-      <Route
-        path="/new-memory"
-        element={<MemoryWriting/>}
-      />
-      <Route
-        path="/keyword"
-        element={<KeyWord/>}
-      />
-      <Route
-        path="/calendar"
-        element={<Intro/>}
-      />
-      <Route
-        path="/memory"
-        element={<Memory/>}
-      />
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/intro" element={<Intro />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/user-setting" element={<UserSettings />} />
+            <Route path="/main-page" element={<MainPage />} />
+            <Route path="/new-jar" element={<AddJar />} />
+            <Route path="/new-memory" element={<MemoryWriting />} />
+            <Route path="/keyword" element={<KeyWord />} />
+            <Route path="/calendar" element={<CalendarPage />} />
+            <Route path="/memory" element={<Memory />} />
+        </Routes>
+    </BrowserRouter>
 );
